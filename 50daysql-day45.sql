e38bbe739c74a6a3ae999dc45bc9bb80ee558e53
Salim Muhammed <salimpunnakkan313@gmail.com>
2024-06-17 22:47:31 +0530

    Add files via upload

---
+++ 50daysql-day45.sql
@@ -0,0 +1,82 @@
+-- SQL Challenge 45/50
+
+DROP TABLE IF EXISTS orders;
+-- Create Orders table
+CREATE TABLE Orders (
+    Order_id INT PRIMARY KEY,
+    Customer_id INT,
+    Order_Date DATE,
+    Amount DECIMAL(10, 2)
+);
+
+
+DROP TABLE IF EXISTS Customer;
+-- Create Customers table
+CREATE TABLE Customer (
+    Customer_id INT PRIMARY KEY,
+    Customer_Name VARCHAR(50),
+    Join_Date DATE
+);
+
+-- Insert records into Orders table
+INSERT INTO Orders (Order_id, Customer_id, Order_Date, Amount)
+VALUES
+    (1, 1, '2024-05-01', 100),
+    (2, 2, '2024-05-02', 150),
+    (3, 3, '2023-12-15', 200),
+    (4, 1, '2024-05-03', 120),
+    (5, 2, '2024-01-20', 180),
+    (6, 4, '2024-03-10', 90);
+
+-- Insert records into Customers table
+INSERT INTO Customer (Customer_id, Customer_Name, Join_Date)
+VALUES
+    (1, 'Alice', '2024-01-15'),
+    (2, 'Bob', '2024-02-20'),
+    (3, 'Charlie', '2023-12-01'),
+    (4, 'David', '2024-03-01');
+
+
+
+/*
+-- Amazon Data Analyst interview 
+questions for exp 1-3 year!
+
+You have two tables: Orders and Customers.
+
+ - Orders Table Columns: 
+Order_id, Customer_id, Order_Date, Amount
+
+- Customers Table Columns: 
+Customer_id, Customer_Name, Join_Date
+ 
+Write an SQL query to calculate the total 
+order amount for each customer who joined 
+in the current year. 
+
+The output should contain Customer_Name and 
+the total amount.
+
+*/
+-- join both table based on cx id
+-- filter the cx records for current_year 
+-- based on eacx cx id sum the amount
+-- group by cx id
+
+
+SELECT * FROM Customer;
+SELECT * FROM orders;
+
+select 
+     --c.customer_id,
+	 c.customer_name,
+	 sum(o.amount) as total_amt
+from orders o
+join customer c
+on o.customer_id = c.customer_id
+where extract(year from c.join_date) = extract(year from current_date)
+group by 1;
+
+
+
+
